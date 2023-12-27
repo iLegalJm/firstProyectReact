40c18eac8ec3ca37fdffcd6d9d385b5349cdfa69
iLegalJm <josemcz2211@gmail.com>
2023-12-26 21:50:08 -0500

    logData

--- src/Components/UsersList.jsx
+++ src/Components/UsersList.jsx
@@ -9,6 +9,7 @@ export const UsersList = ({ endPoint }) => {
                 const response = await fetch(`https://jsonplaceholder.typicode.com/${endPoint}`);
                 const data = await response.json();
                 setData(data);
+                console.log(data);
             } catch (e) {
                 console.log(e);
             }
